d83d2d9a94b4b0ce8d7e8c1696ce1b3aed11fc8e
frkxo <62440451+frkxo@users.noreply.github.com>
2020-06-25 16:55:25 +0300

    Create CameraFollow.cs

---
+++ CameraFollow.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform target;
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        if(target == null) return;
+        transform.position = new Vector3(target.position.x,
+                                         transform.position.y,
+                                         transform.position.z);
+    }
+}
